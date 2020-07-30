c51dbc780bfbf7542b28e78971d1056c36494dae
youyou <leigned@outlook.com>
2020-07-30 13:37:09 +0800

    添加配置的注释

--- assets/config.ts
+++ assets/config.ts
@@ -1,18 +1,18 @@
-let resConfig = {
-    altasArr: ['main'],
-    jsonArr: [],
-    prefabArr: ['bullet', 'zergling'],
+let resConfig = {//填写Resrouce下需要自动加载的资源
+    altasArr: ['main'],//自动图集加载 altas/下的图集名字 然后在ResType的enum下新增一个枚举，使用的时候使用ResouceManager.instance.getSprite(新的枚举,图集中图片的名字字符串)
+    jsonArr: [],//自动加载json配表 填写文件名 使用时使用JsonManager.instance.getDataByName(名字)
+    prefabArr: ['bullet', 'zergling'],//自动图集加载预制体 使用时用PoolManager.instance.create/removeObjectByName(预制体名字，父节点/当前节点)
 }
-let uiName = {
+let uiName = {//填写Resrouce/ui下需要自动加载的ui 动态打开实例代码:UIManager.instance.openUI(WinUIManager, { name: config.uiName.winPage })
     messageBox: 'messagebox_ui',
     winPage: 'winPage',
     failPage: 'failPage'
 }
-let animaParams = {
+let animaParams = {//填写Resrouce/anima下需要自动加载的动画配置
     'player_run': {
-        sample: 3,
-        speed: 3,
-        wrapMode: cc.WrapMode.Loop
+        sample: 3,//一秒有几帧  
+        speed: 3,//速度
+        wrapMode: cc.WrapMode.Loop//循环模式
     },
     'monster_run': {
         sample: 2,
@@ -22,12 +22,12 @@ let animaParams = {
     'monster_die': {
         sample: 3,
         speed: 3,
-        wrapMode: cc.WrapMode.Loop,
-        funcs: [
+        wrapMode: cc.WrapMode.Default,
+        funcs: [//动画回调函数
             {
-                frame: 3,
-                func: 'putInPool',
-                param: []
+                frame: 3,//第几帧的时候调用
+                func: 'putInPool',//调用的函数名称 （动画组件和自定义组件必须在同一个node上才能正常调用
+                param: []//调用函数时的参数
             }
         ]
     }
